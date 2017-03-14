2a391f44e0aa4814a3a061d66297831800fc9463
cjredmond <connor.redmond@gmail.com>
2017-03-14 15:32:01 -0400

    added digest to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   config.eager_load = true
   config.serve_static_assets = true
   config.assets.compile = true
+  config.assets.digest = true
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
