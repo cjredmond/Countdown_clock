e94bc47630d9078e6e5ae068120910b3ab5e8784
cjredmond <connor.redmond@gmail.com>
2017-03-14 15:41:01 -0400

    added /public/ to background controller

--- app/controllers/background_controller.rb
+++ app/controllers/background_controller.rb
@@ -1,5 +1,5 @@
 class BackgroundController < ApplicationController
   def image
-    redirect_to "/assets/background_#{rand(10)}.jpg"
+    redirect_to "public/assets/background_#{rand(10)}.jpg"
   end
 end
